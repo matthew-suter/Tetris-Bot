81784d87004a79532dcb657341f2d0f11c445e49
Matt Suter <mattrsuter@gmail.com>
2024-04-11 15:21:21 +1200

    traingin testing

--- tetris_actor_critic.py
+++ tetris_actor_critic.py
@@ -314,12 +314,12 @@ def train_model(model: TetrisActorCritic, save_filename):
   print(f'\nSolved at episode {i}: average reward: {running_reward:.2f}!')
 
   model.save(save_filename)
-  print(f"Saved model to {save_filename}")
+  print(f"Saved model to {save_filename}\n")
 
 
 min_episodes_criterion = 100
-max_episodes = 1000 #10000
-max_steps_per_episode = 500
+max_episodes = 100 #10000
+max_steps_per_episode = 20000
 
 save_filename = "tetris_model.keras"
 
