5fdafa7e016af57c2f4e53fef60a07a6d98248cf
Matt Suter <mattrsuter@gmail.com>
2024-04-11 15:22:37 +1200

    gfgfdhjkd

--- tetris_actor_critic.py
+++ tetris_actor_critic.py
@@ -318,7 +318,7 @@ def train_model(model: TetrisActorCritic, save_filename):
 
 
 min_episodes_criterion = 100
-max_episodes = 100 #10000
+max_episodes = 1000 #10000
 max_steps_per_episode = 20000
 
 save_filename = "tetris_model.keras"
