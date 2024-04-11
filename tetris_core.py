c01b30f14e2d05dbca7c4b08aff3861b5759d7a1
Matt Suter <mattrsuter@gmail.com>
2024-04-11 12:17:21 +1200

    stealing blackjack code

--- tetris_core.py
+++ tetris_core.py
@@ -1,21 +1,76 @@
+# From Q-learning example
+from __future__ import annotations
+
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from matplotlib.patches import Patch
+from tqdm import tqdm
+
+
+# Other imports
 import gymnasium as gym
 from tetris_agent import *
+
 
 env = gym.make("ALE/Tetris-v5", render_mode="human", obs_type="grayscale")
 observation, info = env.reset()
 
-player = TetrisAgent()
-print(player)
+# player = TetrisAgent()
+# print(player)
 
-for _ in range(1000):
-    action = env.action_space.sample()  # agent policy that uses the observation and info
-    observation, reward, terminated, truncated, info = env.step(action)
 
-    # print(observation.shape)
-    # print()
-    # print(observation)
+learning_rate = 0.01
+n_episodes = 100_000
+start_epsilon = 1.0
+epsilon_decay = start_epsilon / (n_episodes / 2)  # reduce the exploration over time
+final_epsilon = 0.1
 
-    if terminated or truncated:
-        observation, info = env.reset()
+agent = TetrisAgent(
+    learning_rate=learning_rate,
+    initial_epsilon=start_epsilon,
+    epsilon_decay=epsilon_decay,
+    final_epsilon=final_epsilon,
+)
 
-env.close()
+env = gym.wrappers.RecordEpisodeStatistics(env, deque_size=n_episodes)
+for episode in tqdm(range(n_episodes)):
+    obs, info = env.reset()
+    done = False
+
+    # play one episode
+    while not done:
+        action = agent.get_action(obs)
+        next_obs, reward, terminated, truncated, info = env.step(action)
+
+        # update the agent
+        agent.update(obs, action, reward, terminated, next_obs)
+
+        # update if the environment is done and the current obs
+        done = terminated or truncated
+        obs = next_obs
+
+    agent.decay_epsilon()
+
+
+
+
+
+
+
+
+
+# for _ in range(1000):
+#     action = env.action_space.sample()  # agent policy that uses the observation and info
+#     observation, reward, terminated, truncated, info = env.step(action)
+
+#     # print(observation.shape)
+#     # print()
+#     # print(observation)
+
+#     if terminated or truncated:
+#         observation, info = env.reset()
+
+# env.close()
