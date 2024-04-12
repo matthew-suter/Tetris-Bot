de8bd0799bf4568affd599163d0649b7d7a85a47
Matt Suter <mattrsuter@gmail.com>
2024-04-12 16:20:52 +1200

    Generation WIP

--- genetic_algo.py
+++ genetic_algo.py
@@ -8,67 +8,117 @@ observation, info = env.reset()
 
 # Hyperparameters
 num_hidden_units = 50
+mutation_factor = 0.01
+num_actors = 10
+best_keep = 5 # Keep the best N actors to the next generation, kill the rest
+
+num_generations = 5
 
 @tf.keras.utils.register_keras_serializable()
 class TetrisActor(tf.keras.Model):
-  """GA_Suitable Actor"""
+    """GA_Suitable Actor"""
 
-  def __init__(
-      self,
-      num_actions: int,
-      num_hidden_units: int):
-    """Initialize."""
-    super().__init__()
+    def __init__(
+            self,
+            num_actions: int,
+            num_hidden_units: int):
+        """Initialize."""
+        super().__init__()
 
-    self.dense1 = layers.Dense(num_hidden_units, activation="relu")
-    self.dense2 = layers.Dense(num_actions)
+        self.dense1 = layers.Dense(num_hidden_units, activation="relu")
+        self.dense2 = layers.Dense(num_actions)
 
-  def call(self, inputs: tf.Tensor) -> tf.Tensor:
-    x = self.dense1(inputs)
-    return self.dense2(x)
-  
-  def get_config(self):
-    # Get the base configuration from the parent class
-    config = super().get_config()
-    # Add custom parameters to the config
-    config['num_actions'] = self.dense2.units
-    config['num_hidden_units'] = self.dense1.units
-    return config
+
+    def call(self, inputs: tf.Tensor) -> tf.Tensor:
+        x = self.dense1(inputs)
+        return self.dense2(x)
+
+
+    def shuffle_weights(self, shuffle_factor=0.01):
+        """Shuffles the values of all layers' weights"""
+        dense1_weights = self.dense1.get_weights()
+        self._shuffle_layer_weights(dense1_weights, shuffle_factor)
+        self.dense1.set_weights(dense1_weights)
+
+        dense2_weights = self.dense2.get_weights()
+        self._shuffle_layer_weights(dense2_weights, shuffle_factor)
+        self.dense2.set_weights(dense2_weights)
+
+
+    def _shuffle_layer_weights(self, layer_weights, shuffle_factor):
+        """Layer weights are a python list of numpy arrays. Gross, this scales each value by a random factor"""
+        for i in range(len(layer_weights)):
+            scale_factor = tf.random.uniform(layer_weights[i].shape, minval=1-shuffle_factor, maxval=1+shuffle_factor)
+            scaled = layer_weights[i] * scale_factor
+            layer_weights[i] = scaled
+
+
+    def get_config(self):
+        # Get the base configuration from the parent class
+        config = super().get_config()
+        # Add custom parameters to the config
+        config['num_actions'] = self.dense2.units
+        config['num_hidden_units'] = self.dense1.units
+        return config
 
 
 def greyscale_to_one_hot(greyscale):
     result =  greyscale[28:204:8, 24:64:4] # Trim to only include the playspace, shifting one pixel down and left to avoid inter-block gaps, and then downscale
     result = (result != 0x6F)*1 # Check if background colour (Hex code 0x6F), and convert to numerical boolean using disgusting typecasting
+    result = tf.reshape(result, [1, -1])
     return result
 
 
 
 def training():
-    num_actors = 100
     actors = []
 
     for i in range(num_actors):
         actors.append(TetrisActor(num_actions=env.action_space.n, num_hidden_units=num_hidden_units))
 
-    trial_actors(actors)
+    for i in range(num_generations):
+        print(f"Generation {i+1}")
+
+        # Test all of the actors
+        actor_scores = trial_actors(actors, verbose_printing=True)
+        print(actor_scores)
+
+        # Take the best N actors
+        new_actors = []
+        best_actors_idx = np.argpartition(actor_scores, -best_keep)[-best_keep:]
+        print(best_actors_idx)
+
+        for actor in actors:
+            actor.shuffle_weights(mutation_factor)
 
 
 
-def trial_actors(actors):
+def trial_actors(actors, verbose_printing=False):
+    actor_scores = np.zeros(len(actors))
+
     for i, actor in enumerate(actors):
-        print(f"Training actor #{i}...")
+        if verbose_printing:
+            print(f"Training actor #{i+1}... ", end="")
         greyscale, info = env.reset()
 
         done = False
         truncated = False
+        steps_survived = 0
 
         while not (done or truncated):
             state = greyscale_to_one_hot(greyscale)
             action = actor.call(state)
-            print(action)
-            action = tf.argmax(action)
-            print(action)
+
+            # print(actor.summary())
+            action = int(tf.argmax(action, axis=1))
             greyscale, reward, done, truncated, info = env.step(action)
+            steps_survived += 1
+        
+        if verbose_printing:
+            print(f"Lasted {steps_survived} steps")
+        actor_scores[i] = steps_survived
+    
+    return actor_scores
 
 
 
